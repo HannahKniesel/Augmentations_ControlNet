d9717f06b6f8f7fc28a97348523cd92819b0460b
Hansel <hansel@pop-os.localdomain>
2024-06-13 10:17:05 +0200

    add standard pipeline

--- GenerateData.py
+++ GenerateData.py
@@ -305,7 +305,7 @@ if __name__ == "__main__":
         if((optimization_params["wandb_mode"] in ["detailed"]) or ((optimization_params["wandb_mode"] in ["every_100"]) and ((img_idx%100) == 0))):
             fig,axs = plt.subplots(1,len(augmentations))
             if(len(augmentations) == 1):
-                axs.imshow(a)
+                axs.imshow(augmentations[0])
             else:
                 for j, a in enumerate(augmentations): 
                     axs[j].imshow(a)
